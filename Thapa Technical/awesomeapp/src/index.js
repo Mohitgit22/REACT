9a5e5ebd554386f6a11c35abf4db2e65949fe871
Mohitgit22 <mohit.2022ug1007@iiitranchi.ac.in>
2023-12-22 21:43:14 +0530

    Added js expression {} in jsax elements

--- Thapa Technical/awesomeapp/src/index.js
+++ Thapa Technical/awesomeapp/src/index.js
@@ -1,17 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom"
 
+const channel = "Netflix" 
 ReactDOM.render(
      <>
-      <h1> Thapa Technical Netflix Pick</h1>
-      <p> List of 5 Best Series</p>
-      <ol>
-        <li>Dark</li>
-        <li>Extra curricular</li>
-        <li>My Holo Love</li>
-        <li>My first-2 Love</li>
-        <li>Mr Robot</li>
-      </ol>
+      <h1> Thapa Technical {channel} Pick</h1>
      </>,
      document.getElementById("root")
 )
