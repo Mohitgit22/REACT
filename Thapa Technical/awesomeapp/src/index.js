ea5f893fc2ed29ae69618c9bf5481a222074b608
Mohitgit22 <mohit.2022ug1007@iiitranchi.ac.in>
2023-12-22 22:55:38 +0530

    Added a website showing current date and current time

--- Thapa Technical/awesomeapp/src/index.js
+++ Thapa Technical/awesomeapp/src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom"
 
-const fname = "Mohit";
-const lname = "Verma"
+const currDate = new Date().toLocaleDateString();
+const currtime = new Date().toLocaleTimeString();
+
 ReactDOM.render(
-     <>
-      <h1>{`my name is ${fname} ${lname}`}</h1>
-     </>,
-     document.getElementById("root")
-)
-      
+  <>
+    <h1> Hello , My name is Mohit Verma</h1>
+    <p> Current date is = {currDate}</p>
+    <p> Current time is = {currtime}</p>
+  </>,
+  document.getElementById('root')  
+);
