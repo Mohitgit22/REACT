7613d6d485d9ec00b4be74e5e60bf6a25370054f
Mohitgit22 <mohit.2022ug1007@iiitranchi.ac.in>
2023-12-22 23:35:16 +0530

    Added CSS and used google font

--- Thapa Technical/awesomeapp/src/index.js
+++ Thapa Technical/awesomeapp/src/index.js
@@ -1,20 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom"
+import './index.css'
 
 const name = "Mohit"
 const img1 = " https://picsum.photos/200/300 ";
 const img2 = " https://picsum.photos/300/300";
-const img3 = " https://picsum.photos/900/300";
+const img3 = " https://picsum.photos/400/300";
 const links = "https://www.google.co.in/?client=safari&channel=mac_bm"
 
 ReactDOM.render(
   <>
-    <h1 contentEditable = "true"> Hello , My name is {name}</h1>
+    <h1 className = "heading"> Hello , My name is {name}</h1>
+    <div className="img_div">
     <img src = {img1} alt="random images" />
     <img src = {img2} alt="random images" />
     <a href = {links} target="_blank">
     <img src = {img3} alt="random images" />
     </a>
+    </div>
   </>,
   document.getElementById('root')  
 );
