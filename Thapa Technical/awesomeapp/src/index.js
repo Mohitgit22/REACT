80396e1effd5601ebc9240770f54152587d3c964
Mohitgit22 <mohit.2022ug1007@iiitranchi.ac.in>
2023-12-22 23:11:08 +0530

    Added image and link and saw camel case usage in react

--- Thapa Technical/awesomeapp/src/index.js
+++ Thapa Technical/awesomeapp/src/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom"
 
-const currDate = new Date().toLocaleDateString();
-const currtime = new Date().toLocaleTimeString();
+const name = "Mohit"
+const img1 = " https://picsum.photos/200/300 ";
+const img2 = " https://picsum.photos/300/300";
+const img3 = " https://picsum.photos/900/300";
+const links = "https://www.google.co.in/?client=safari&channel=mac_bm"
 
 ReactDOM.render(
   <>
-    <h1> Hello , My name is Mohit Verma</h1>
-    <p> Current date is = {currDate}</p>
-    <p> Current time is = {currtime}</p>
+    <h1 contentEditable = "true"> Hello , My name is {name}</h1>
+    <img src = {img1} alt="random images" />
+    <img src = {img2} alt="random images" />
+    <a href = {links} target="_blank">
+    <img src = {img3} alt="random images" />
+    </a>
   </>,
   document.getElementById('root')  
 );
