b8a06a9820043d29729d8bb2e9fa63c171a7e8fb
Mohitgit22 <mohit.2022ug1007@iiitranchi.ac.in>
2023-12-23 12:13:18 +0530

    Added inline CSS and Internal CSS

--- Thapa Technical/awesomeapp/src/index.js
+++ Thapa Technical/awesomeapp/src/index.js
@@ -8,9 +8,29 @@ const img2 = " https://picsum.photos/300/300";
 const img3 = " https://picsum.photos/400/300";
 const links = "https://www.google.co.in/?client=safari&channel=mac_bm"
 
+
+// .heading {
+//   color: #fa9191;
+//   text-align: center;
+//   text-transform: capitalize;
+//   font-weight: bold;
+//   margin: 50px 0px;
+//   font-family: 'Varela Round', sans-serif;
+// }
+
+const heading = {
+  color: "#fa1313",
+  textTransform: "capitalize",
+  fontWeight: " bold ",
+  textAlign: "center",
+  margin: "50px 0px",
+  textShadow: "0pc 2px 4px #ffe9c5",
+  fontFamily: "'Varela Round', sans-serif",
+   } 
+
 ReactDOM.render(
   <>
-    <h1 className = "heading"> Hello , My name is {name}</h1>
+    <h1 style={ heading }> Hello , My name is {name}</h1>
     <div className="img_div">
     <img src = {img1} alt="random images" />
     <img src = {img2} alt="random images" />
