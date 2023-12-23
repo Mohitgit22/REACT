564a645e38930adfd723fd9a084081cfafdf7ca8
Mohitgit22 <mohit.2022ug1007@iiitranchi.ac.in>
2023-12-23 21:25:42 +0530

    Created a top netflix series website using react

---
+++ Thapa Technical/ProjectWork/netfilxapp-react/src/Sdata.jsx
@@ -0,0 +1,34 @@
+const Sdata = [
+    {
+        imgsrc: "https://upload.wikimedia.org/wikipedia/en/d/da/DarkNetflixPosterEnglish.jpg",
+        title: " A Netflix Original Series",
+        sname: "DARK",
+        link: "https://www.netflix.com/in/title/80100172"
+    },
+    {
+        imgsrc: "https://occ-0-2794-2219.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABcsNRTCR_xEGk0JKJnrf-zVc7eZYzLh3sojYa1Rude8R_3jboyMRsOysg_b5r0zg69rO15TU_T7aI0lKTy-XVda4em0FsaNpkTcL.jpg?r=15c",
+        title: " A Netflix Original Series",
+        sname: "PEAKY BLINDERS",
+        link: "https://www.netflix.com/in/title/80002479"
+    },
+    {
+        imgsrc: "https://occ-0-2794-2219.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABWMP8gHYkc4P-yFlZyg5Ku2DooykoZumTBHbwZ-8YcMVFCAMnsW84lxLad770e2k7-nURGuzhhsXX_pDaUeU7H8-uP_HCw__V7kRTrj-ptgEPbXdDgbxFez6.jpg?r=283",
+        title: " A Netflix Original Series",
+        sname: "MONEY HEIST",
+        link: "https://www.netflix.com/in/title/80192098"
+    },
+    {
+        imgsrc: "https://occ-0-2794-2219.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABVqcLQpf6mxvyIfcaadFQhph_iN1tmNUjmHziE4hXp6HsNL0ViweqrNA3JUqdX_ukwGIWZzli2G4cry-1PYBFRk22_a8C2LG86Cj.jpg?r=1ef",
+        title: " A Netflix Original Series",
+        sname: "EXTRACTION",
+        link: "https://www.netflix.com/in/title/80230399"
+    },
+    {
+        imgsrc: "https://occ-0-993-586.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABV5lteI3eHet1nPBQKC_uEChESjqQfYpRwMWLR0wULt52odnxQtG69JNFDj9N4maZWJWDFZyi2sc1YzZ8mKE4Ajmt7Btva1CnWagGbYXVMp3t4OETM4LwiD70dHu-qWUbBAesQ.jpg?r=c68",
+        title: " A Netflix Original Series",
+        sname: "STRANGER THINGS",
+        link: "https://www.netflix.com/in/title/80057281"
+    }
+]
+
+export default Sdata;
