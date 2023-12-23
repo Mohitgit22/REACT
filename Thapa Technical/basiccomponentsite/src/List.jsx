f47107a32aeb17a3084393d1af6b3f12e84e6834
Mohitgit22 <mohit.2022ug1007@iiitranchi.ac.in>
2023-12-23 14:44:19 +0530

    Added a basic Component Website

---
+++ Thapa Technical/basiccomponentsite/src/List.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function List () {
+    return (
+        <>
+            <ol>
+            <li>Mohit </li>
+            <li>Suraj</li>
+            <li>Arion</li>
+            <li>Sahil</li>
+            </ol>
+        </>
+    )
+}
+
+export default List;
