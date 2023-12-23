625e300f759153d2ecc0a81682088f28c4e2a536
Mohitgit22 <mohit.2022ug1007@iiitranchi.ac.in>
2023-12-23 21:36:32 +0530

    Completed the netflix website using map in react

--- Thapa Technical/ProjectWork/netfilxapp-react/src/index.js
+++ Thapa Technical/ProjectWork/netfilxapp-react/src/index.js
@@ -5,41 +5,22 @@ import Card from "./Card";
 import './index.css';
 import Sdata from "./Sdata";
 
-
+function ncard(val) {
+  return ( 
+    <Card 
+    imgsrc={val.imgsrc}
+    title={val.title}
+    sname={val.sname}
+    link={val.link}
+  />
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
       <h1 className="Heading" > List of top 5 Netflix Series in 2023 </h1>
-
-    <Card 
-      imgsrc={Sdata[0].imgsrc}
-      title={Sdata[0].title}
-      sname={Sdata[0].sname}
-      link={Sdata[0].link}
-    />
-    <Card 
-      imgsrc={Sdata[1].imgsrc}
-      title={Sdata[1].title}
-      sname={Sdata[1].sname}
-      link={Sdata[1].link}
-    />
-    <Card 
-      imgsrc={Sdata[2].imgsrc}
-      title={Sdata[2].title}
-      sname={Sdata[2].sname}
-      link={Sdata[2].link}
-    />
-    <Card
-    imgsrc={Sdata[3].imgsrc}
-      title={Sdata[3].title}
-      sname={Sdata[3].sname}
-      link={Sdata[3].link} />
-    <Card
-    imgsrc={Sdata[4].imgsrc}
-      title={Sdata[4].title}
-      sname={Sdata[4].sname}
-      link={Sdata[4].link} />
+      {Sdata.map(ncard)};
   </>
 );
 
